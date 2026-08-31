ad8e79c6d496dee027235da0fbebb2be2b4b897f
roy-2582016 <2582016@unai.edu>
2026-08-31 15:42:29 +0700

    Biodata Diri

---
+++ src/BiodataDiri.java
@@ -0,0 +1,8 @@
+public class BiodataDiri {
+    public static void main(String[] args) {
+        String nama = "Roy Sion Manalu";
+        String nim  = "2582016";
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+    }
+}
